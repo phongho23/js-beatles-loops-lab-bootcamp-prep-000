b267f01369db3d858807a2166f5b0145e3084277
Phong Ho <ho.phongt@gmail.com>
2018-03-24 18:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,23 +21,7 @@ function johnLennonFacts(){
   const lennonFacts = [];
   let i = 0;
   while (i < facts.length) {
-    lennonFacts.push(`${facts[1]} + " !!!"`)
-  }
+    lennonFacts.push(`${facts[i]} + " !!!"`)
   }
 }
 
-  describe('johnLennonFacts', function(){
-    it("returns an array of strings with exclamation points", function() {
-      expect(johnLennonFacts([
-        "He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"
-      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
-
-      expect(johnLennonFacts([
-        "foo",
-        "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
-    });
-  });
