9066f89756507e18c59e1106a7ad0c4a2f7999d4
Phong Ho <ho.phongt@gmail.com>
2018-03-24 18:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function theBeatlesPlay(musicians, instruments){
     return beatlesPlay;
 }
 
+function johnLennonFacts(){
+  
+}
+
+  describe('johnLennonFacts', function(){
+    it("returns an array of strings with exclamation points", function() {
+      expect(johnLennonFacts([
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+
+      expect(johnLennonFacts([
+        "foo",
+        "bar",
+      ])).toEqual(["foo!!!", "bar!!!"])
+    });
+  });
